4577f20a14bb27f0be7c8535ea73a8a9d489f42e
Elise Brun <elisztomania@gmail.com>
2020-11-12 14:41:52 +0100

    adding seeds with the parsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,32 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.firstputs 'Cleaning database...'
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+# lemon = { name: "lemon" }
+# ice = { name: "ice" }
+# mint_leaves = { name: "mint_leaves" }
+# vodka = { name: "vodka" }
+
+# [lemon, ice, mint_leaves, vodka].each do |attributes|
+#   ingredient = Ingredient.create!(attributes)
+#   puts "Created #{ingredient.name}"
+# end
+
+puts 'Creating restaurants...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients['drinks'].each do |attributes|
+  ingredient = Ingredient.create!(name: attributes['strIngredient1'])
+  puts "Created #{ingredient.name}"
+end
+
+puts 'Finished!'
